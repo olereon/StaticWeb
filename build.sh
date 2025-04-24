694b9095a0fb6cabd3a6f8254b810b13d9ad4967
olereon <shyraoleg@outlook.com>
2025-04-25 00:06:10 +0200

    repo name in build + gitignore

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ set -e
 echo "--- Running Production Build ---"
 
 # Replace YourRepoName with the actual name of your GitHub repository
-REPO_NAME="StaticWeb" # <<< CHANGE THIS TO YOUR REPO NAME
+REPO_NAME="https://github.com/olereon/StaticWeb" # <<< CHANGE THIS TO YOUR REPO NAME
 
 # Run the main script, passing the base path as an argument
 # Ensure the base path starts and ends with a slash
-python3 src/main.py "/${https://github.com/olereon/StaticWeb,}/"
+python3 src/main.py "/${REPO_NAME}/"
 
 echo "--- Build Complete ---"
 echo "Generated site placed in 'docs' directory."
